e7b4e465eae37715f054fdb7ee376e91c91d416c
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-16 15:14:37 +0000

    add async version of useEffect in Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -23,7 +23,30 @@ export default function Meme() {
       });
   }, []);
 
-  console.log(allMemes);
+  // console.log(allMemes);
+
+  /*
+  * Same but using async
+
+  React.useEffect(() => {
+    async function getMemes() {
+        const res = await fetch("https://api.imgflip.com/get_memes")
+        const data = await res.json()
+        setAllMemes(data.data.memes)
+    }
+    getMemes()
+}, [])
+
+/*
+/*
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen above.
+    */
 
   const getRandomImage = () => {
     const randomIndex = Math.floor(Math.random() * allMemes.length);
