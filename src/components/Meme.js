37bb3198f43372cf9715f83f5179f28caa5e7998
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-16 14:46:11 +0000

    Start implementing call to Api using fetch()

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/MemeStyle.css";
-import { memesData } from "../data/memesData";
+// import { memesData } from "../data/memesData";
 
 export default function Meme() {
   // const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg");
@@ -11,12 +11,23 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = useState(memesData);
+  // const [allMemes, setAllMemes] = useState(memesData);
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllMemes(data.data.memes);
+      });
+  }, []);
+
+  console.log(allMemes);
 
   const getRandomImage = () => {
-    const memesArray = allMemeImages.data.memes;
-    const randomIndex = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomIndex].url;
+    const randomIndex = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomIndex].url;
     //access previous meme (l.8) and alter it's img property to render the new random img instead
     setMeme((prevMeme) => ({
       ...prevMeme,
@@ -24,22 +35,39 @@ export default function Meme() {
     }));
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      // return a new object based on previous meme obj then updated accordingly
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <>
-      <form className="">
+      <form className="form">
         <div className="row justify-content-center  pt-5 pb-4">
           <div className="col-sm-5 ">
             <input
               type="text"
               className="form-control"
-              placeholder="First name"
+              placeholder="Top text"
+              name="topText"
+              value={meme.topText}
+              onChange={handleChange}
             />
           </div>
           <div className="col-sm-5 ">
             <input
               type="text"
               className="form-control"
-              placeholder="Last name"
+              placeholder="Bottom text"
+              name="bottomText"
+              value={meme.bottomText}
+              onChange={handleChange}
             />
           </div>
         </div>
@@ -53,12 +81,20 @@ export default function Meme() {
           </button>
         </div>
       </form>
-      <div className="row">
+      <section className="row " id="meme-section">
         <img
-          className="meme-img col-sm-8 mx-auto my-5"
+          className="col-sm-8 mx-auto my-5"
+          id="meme-img"
           src={meme.randomImage}
+          alt="This is a meme"
         />
-      </div>
+        <h2 className="meme-text pt-5" id="top-text">
+          {meme.topText}
+        </h2>
+        <h2 className="meme-text pb-5" id="bottom-text">
+          {meme.bottomText}
+        </h2>
+      </section>
     </>
   );
 }
